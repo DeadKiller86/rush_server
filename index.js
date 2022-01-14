87c4455646d7af3e591d1c02c35f5606ebe9a10e
DeadKiller86 <87462053+DeadKiller86@users.noreply.github.com>
2022-01-14 22:32:03 +0530

    asdfasdf

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config({ path: "./config.env" });
+const cors = require("cors")
 
 const express = require("express");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/helplinenumber", require("./routes/main"));
 app.listen(PORT, () =>
